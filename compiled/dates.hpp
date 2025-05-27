6dcb74962d7dfbc656f440034887bdcae3ca9df3
Alexey Tereshenkov <50622389+AlexTereshenkov@users.noreply.github.com>
2025-05-27 15:27:03 +0200

    bazel: add cc_import target with a static library (#10)

--- compiled/dates.hpp
+++ compiled/dates.hpp
@@ -1,7 +1,19 @@
-// implementation is stored in sandbox/dates.cpp file;
-// to compile into an .so file, run:
-// $ g++ -fPIC -shared -s -Wl,--strip-all -o compiled/dates.so sandbox/dates.cc
-// $ strip --strip-all compiled/dates.so
+/*
+Implementation is stored in sandbox/dates.cpp file.
+
+To compile into an .so file (shared library), run:
+$ g++ -fPIC -shared -s -Wl,--strip-all -o compiled/dates.so sandbox/dates.cc
+$ strip --strip-all compiled/dates.so
+
+To compile into an .a file (static library), run:
+$ g++ -c -o compiled/dates.o sandbox/dates.cc
+$ ar rcs compiled/libdates.a compiled/dates.o
+$ strip --strip-debug compiled/libdates.a
+$ rm compiled/dates.o
+
+Keep in mind that it's necessary to use the same compiler
+Bazel uses (the host or the toolchain) and the same flags.
+*/
 
 #ifndef DATES_HPP
 #define DATES_HPP
